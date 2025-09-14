030cbf72e3dba45dc9c1f3ed8ac39a65d18a9a89
ognjenstanic22 <ogizemun1@gmail.com>
2025-09-14 22:55:07 +0200

    Added search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,71 @@
-import { NgIf, SlicePipe } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MovieModel } from '../../models/movie.model';
 import { MovieService } from '../../services/movie.service';
 import { LoadingComponent } from "../loading/loading.component";
 import { RouterLink } from '@angular/router';
 import { UtilsService } from '../../services/utils.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { DirectorModel } from '../../models/director.model';
 
 @Component({
   selector: 'app-search',
-  imports: [MatTableModule, NgIf, MatButtonModule, LoadingComponent, RouterLink],
+  imports: [
+    MatTableModule,
+    NgIf,
+    NgFor,
+    MatButtonModule,
+    LoadingComponent,
+    RouterLink,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    MatSelectModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
   displayedColumns: string[] = ['movieId', 'title', 'runTime', 'director.name', 'price', 'actions'];
+  allData: MovieModel[] | null = null
+  directors: DirectorModel[] = []
+  selectedDirector: string | null = ''
   dataSource: MovieModel[] | null = null
 
-  public constructor(public utils: UtilsService){
+  public constructor(public utils: UtilsService) {
     MovieService.getMovies()
-      .then(rsp => this.dataSource = rsp.data)
+      .then(rsp => {
+        this.allData = rsp.data
+        this.dataSource = rsp.data
+        this.directors = rsp.data.map((obj: MovieModel) => obj.director)
+      })
+  }
+
+  public doSearch(e: any) {
+    const input = e.target.value
+
+    if (this.allData == null) return
+
+    if (input == '') {
+      this.dataSource = this.allData
+      return
+    }
+
+    this.dataSource = this.allData.filter(obj => {
+      return obj.title.includes(input) ||
+        obj.runTime.toString().includes(input) ||
+        obj.director.name.includes(input)
+    })
+  }
+
+  public doSelectDirector(e: any){
+    this.dataSource = this.allData!.filter( obj => obj.director.name == this.selectedDirector)
   }
 }
